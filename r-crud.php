a7ec83e49f1419a75db00deade68b1aac113ee90
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2021-03-04 18:32:16 +0700

    Update r-crud.php

--- r-crud.php
+++ r-crud.php
@@ -27,6 +27,7 @@ class r_crud
     public function data_read($connection, $table)
     {
 
+        $table = mysqli_real_escape_string($connection, $table);
         $query = "SELECT * FROM $table";
         $execute = mysqli_query($connection, $query);
         $data = mysqli_fetch_array($execute);
@@ -37,6 +38,8 @@ class r_crud
     public function data_read_by_id($connection, $table, $id)
     {
 
+        $id = mysqli_real_escape_string($connection, $id);
+        $table = mysqli_real_escape_string($connection, $table);
         $query = "SELECT * FROM $table WHERE id='$id'";
         $execute = mysqli_query($connection, $query);
         $data = mysqli_fetch_array($execute);
@@ -56,7 +59,9 @@ class r_crud
     public function data_update($connection, $table, $data, $id)
     {
 
-        $struct_data = implode(', ', array_map(function ($v, $k) { return sprintf("%s=\"%s\"", $k, $v); }, $data, array_keys($data)));
+        $id = mysqli_real_escape_string($connection, $id);
+        $table = mysqli_real_escape_string($connection, $table);
+        $struct_data = implode(', ', array_map(function ($v, $k) { return sprintf("%s=\"%s\"", $k, mysqli_real_escape_string($connection, $v)); }, $data, array_keys($data)));
         $query = "UPDATE $table SET $struct_data WHERE id='$id'";
         $execute = mysqli_query($connection, $query);
 
@@ -75,6 +80,7 @@ class r_crud
     public function data_delete($connection, $table, $id)
     {
 
+        $id = mysqli_real_escape_string($connection, $id);
         $query = "DELETE FROM $table WHERE id='$id'";
         $execute = mysqli_query($connection, $query);
 
