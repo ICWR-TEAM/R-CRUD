a483fa99604002ffa71947418cc79fda1959b817
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2021-03-09 16:31:16 +0700

    Update r-crud.php

--- r-crud.php
+++ r-crud.php
@@ -24,12 +24,40 @@ class r_crud
 
     }
 
+    public function data_read($connection, $table)
+    {
+
+        $query = "SELECT * FROM $table";
+        $execute = mysqli_query($connection, $query);
+        $data = [];
+        $no = 1;
+
+        while ($raw_data = mysqli_fetch_array($execute)) {
+
+            $data[$no++] = $raw_data;
+
+        }
+
+        return $data;
+
+    }
+
+    public function data_read_by_id($connection, $table, $id)
+    {
+
+        $query = "SELECT * FROM $table WHERE id='$id'";
+        $execute = mysqli_query($connection, $query);
+        $data = mysqli_fetch_array($execute);
+        return $data;
+
+    }
+
     public function data_update($connection, $table, $data, $id)
     {
 
         $id = mysqli_real_escape_string($connection, $id);
         $table = mysqli_real_escape_string($connection, $table);
-        $struct_data = implode(', ', array_map(function ($v, $k) { return sprintf("%s=\"%s\"", $k, mysqli_real_escape_string($connection, $v)); }, $data, array_keys($data)));
+        $struct_data = implode(', ', array_map(function ($v, $k) { return sprintf("%s=\"%s\"", $k, $v); }, $data, array_keys($data)));
         $query = "UPDATE $table SET $struct_data WHERE id='$id'";
         $execute = mysqli_query($connection, $query);
 
